f66701d1e425df97cdf24323b75ecb027c968c3c
ladganesh <43412395+ladganesh@users.noreply.github.com>
2019-12-19 18:12:39 +0530

    updated server information

--- disk_usage.sh
+++ disk_usage.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+echo "------------Server details---------"
+echo "1. Server ip : " `ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+echo "2. Hostname : " `hostname`
+echo "3. OS information : " `cat /etc/redhat-release` `cat /etc/issue`
+echo "4. Total virtual CPU : " `nproc` 
+echo "5. Total Memory in GB : "`free -h | awk '/Mem\:/ { print $2 }'` 
+
+
 BLOCKS=0
 
 # The special /sys/ folder contains device information, not actual files.
 
 DISK=`ls -l  /sys/class/block/sd*`
 diskopt=$?
-#if [ diskopt == 0 ]
 if [[ diskopt -eq 0 ]]
 then
 for DEV in /sys/class/block/sd*
@@ -15,7 +22,7 @@ do
         read B < $DEV/size                      # Read num of blocks
         read MODEL < $DEV/device/model          # Read model
 
-        printf "%-40s%15d blocks %5d GB\n" "$MODEL" "$B" $((B/(1024*1024*2) ))
+       # printf "%-40s%15d blocks %5d GB\n" "$MODEL" "$B" $((B/(1024*1024*2) ))
         ((BLOCKS+=B))   # Add to total
 done
 
@@ -31,15 +38,14 @@ do
         printf "%-40s%15d blocks %5d GB\n" "$MODEL" "$B" $((B/(1024*1024*2) ))
         ((BLOCKS+=B))   # Add to total
 done
-#else
-#       echo "Please check HDD"
 
 fi
 
-printf "%-40s%15d blocks %5d GB\n" "total" "$BLOCKS" $((BLOCKS/(1024*1024*2)))
+#printf "%-40s%15d blocks %5d GB\n" "total" "$BLOCKS" $((BLOCKS/(1024*1024*2)))
 
-#ANS = (BLOCKS/(1024*1024*2))
 ANS=$((BLOCKS/(1024*1024*2)))
 
+echo "6. Total Disk storage in GB : $ANS GB " 
 
-echo -e "\n\n TOTAL=$ANS GB"
+echo "7. Total Swap memory in GB : " `free -h | grep Swap | awk '{ print $2 }'`
+

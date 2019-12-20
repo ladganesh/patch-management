8465325b59c85fb5832253998d2bcd37f8679133
ladganesh <43412395+ladganesh@users.noreply.github.com>
2019-12-20 18:23:58 +0530

    updated

--- precheck.sh
+++ precheck.sh
@@ -1,69 +1,76 @@
-echo "========================= Server IP =======================================================" > /root/patching/`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+echo "========================= Time =======================================================" > /root/patching/`ip addr | grep 'state UP' -A2 | head -n3| tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
 
-ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/' >> /root/patching/`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
-
-
-
-echo "======================== fstab Details =========================================================" >> /root/patching/`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
-
-cat /etc/fstab >> /root/patching/`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+date >> /root/patching/`ip addr | grep 'state UP' -A2 | head -n3 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
 
 
 
-echo "========================= Route points ========================================================" >> /root/patching/`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
 
-route -n >> /root/patching/`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+echo "========================= Server IP =======================================================" >> /root/patching/`ip addr | grep 'state UP' -A2 | head -n3| tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
 
-
-
-echo "========================== Enabled Services =======================================================" >> /root/patching/`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
-
-chkconfig --list >> /root/patching/`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+ip addr | grep 'state UP' -A2 | head -n3| tail -n1 | awk '{print $2}' | cut -f1  -d'/' >> /root/patching/`ip addr | grep 'state UP' -A2 | head -n3 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
 
 
 
-echo "=========================== Runing/Lisining ports ======================================================" >> /root/patching/`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+echo "======================== fstab Details =========================================================" >> /root/patching/`ip addr | grep 'state UP' -A2 | head -n3| tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
 
-netstat -ntupl >> /root/patching/`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
-
-
-
-echo "============================ Disk Utilization =====================================================" >> /root/patching/`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
-
-df -hT >> /root/patching/`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+cat /etc/fstab >> /root/patching/`ip addr | grep 'state UP' -A2 | head -n3| tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
 
 
 
-echo "============================= RAM and Swap memory ====================================================" >> /root/patching/`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+echo "========================= Route points ========================================================" >> /root/patching/`ip addr | grep 'state UP' -A2 | head -n3| tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
 
-free -m >> /root/patching/`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
-
-
-
-echo "============================== Server hostname ===================================================" >> /root/patching/`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
-
-hostname --fqdn >> /root/patching/`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
-hostname  >> /root/patching/`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+route -n >> /root/patching/`ip addr | grep 'state UP' -A2 | head -n3| tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
 
 
 
-echo "=============================== Current Mount points ==================================================" >> /root/patching/`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+echo "========================== Enabled Services =======================================================" >> /root/patching/`ip addr | grep 'state UP' -A2 | head -n3| tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
 
-cat /proc/mounts >> /root/patching/`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
-
-
-
-echo "================================ Booting time mount points and routes=================================================" >> /root/patching/`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
-
-cat /etc/rc.local | grep mount >> /root/patching/`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
-cat /etc/rc.local | grep route >> /root/patching/`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+chkconfig --list >> /root/patching/`ip addr | grep 'state UP' -A2 | head -n3| tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
 
 
 
-echo "================================ Network Adopters =================================================" >> /root/patching/`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+echo "=========================== Runing/Lisining ports ======================================================" >> /root/patching/`ip addr | grep 'state UP' -A2 | head -n3| tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
 
-ifconfig >> /root/patching/`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+netstat -ntupl >> /root/patching/`ip addr | grep 'state UP' -A2 | head -n3| tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
 
 
 
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" >> /root/patching/`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+echo "============================ Disk Utilization =====================================================" >> /root/patching/`ip addr | grep 'state UP' -A2 | head -n3| tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+
+df -hT >> /root/patching/`ip addr | grep 'state UP' -A2 | head -n3| tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+
+
+
+echo "============================= RAM and Swap memory ====================================================" >> /root/patching/`ip addr | grep 'state UP' -A2 | head -n3| tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+
+free -m >> /root/patching/`ip addr | grep 'state UP' -A2 | head -n3| tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+
+
+
+echo "============================== Server hostname ===================================================" >> /root/patching/`ip addr | grep 'state UP' -A2 | head -n3| tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+
+hostname --fqdn >> /root/patching/`ip addr | grep 'state UP' -A2 | head -n3| tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+hostname  >> /root/patching/`ip addr | grep 'state UP' -A2 | head -n3| tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+
+
+
+echo "=============================== Current Mount points ==================================================" >> /root/patching/`ip addr | grep 'state UP' -A2 | head -n3| tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+
+cat /proc/mounts >> /root/patching/`ip addr | grep 'state UP' -A2 | head -n3| tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+
+
+
+echo "================================ Booting time mount points and routes=================================================" >> /root/patching/`ip addr | grep 'state UP' -A2 | head -n3| tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+
+cat /etc/rc.local | grep mount >> /root/patching/`ip addr | grep 'state UP' -A2 | head -n3 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+cat /etc/rc.local | grep route >> /root/patching/`ip addr | grep 'state UP' -A2 | head -n3| tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+
+
+
+echo "================================ Network Adopters =================================================" >> /root/patching/`ip addr | grep 'state UP' -A2 | head -n3| tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+
+ifconfig >> /root/patching/`ip addr | grep 'state UP' -A2 | head -n3| tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+
+
+
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" >> /root/patching/`ip addr | grep 'state UP' -A2 | head -n3| tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
